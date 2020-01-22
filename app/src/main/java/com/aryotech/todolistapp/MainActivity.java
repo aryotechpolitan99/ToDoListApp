2743ee3c34998110fb542f6cb8b4cb744be1e294
aryotech <aryofreeyoung@gmail.com>
2020-01-22 17:14:17 +0700

    siapkan data dan buat listview

--- app/src/main/java/com/aryotech/todolistapp/MainActivity.java
+++ app/src/main/java/com/aryotech/todolistapp/MainActivity.java
@@ -4,23 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    ListView listView;
+    EditText edtTodo;
+
+    //1. Siapkan Data
+    ArrayList<String> data = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1. Siapkan data
+        createTodos();
 
-        FloatingActionButton fab = findViewById(R.id.btn_fab);
-        fab.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Toast.makeText(MainActivity.this, "you click on fab", Toast.LENGTH_SHORT).show();
-            }
-        });
+        //2. Buat List View
+        listView = findViewById(R.id.lv_list); // define list view
+
+
+    }
+    // 1. Siapkan Data
+    private void createTodos(){
+        data.add("Coding");
+        data.add("Eat");
+        data.add("Sleep");
+        data.add("Traveling");
     }
 }
