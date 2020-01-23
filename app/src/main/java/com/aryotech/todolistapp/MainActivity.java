8958891ba28106cd970a373b017ec79a298f356b
aryotech <aryofreeyoung@gmail.com>
2020-01-23 12:03:32 +0700

    6. define fab, buat on clicklistener, add method on click fab dan buat alert dialog

--- app/src/main/java/com/aryotech/todolistapp/MainActivity.java
+++ app/src/main/java/com/aryotech/todolistapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.aryotech.todolistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton fab;
+    FloatingActionButton fabAdd;
     ListView listView;
     EditText edtTodo;
 
@@ -38,15 +39,53 @@ public class MainActivity extends AppCompatActivity {
         // 3. Buat Adapter dan masukkan parameter yg dibutuhkan. (context, layout_content,tv,data)
         //      parameter data diambil dari langkah 1.
         arrayAdapter = new ArrayAdapter<String>(this,R.layout.content_list,R.id.tv_todo,data);
+
         // 4. Set Adapter kepada List View
         listView.setAdapter(arrayAdapter);
 
+        // 5. Define FAB dan buat onClickListener nya.
+        fabAdd = findViewById(R.id.btn_fab);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //6. Method di bawah ini dibuat sendiri di bawah
+                onClickFabAdd();
+            }
+        });
     }
-    // 1. Siapkan Data
+
+    //1. siapkan data
     private void createTodos(){
-        data.add("Coding");
-        data.add("Eat");
-        data.add("Sleep");
-        data.add("Traveling");
+        data.add("eat");
+        data.add("pray");
+        data.add("sport");
+        data.add("break");
     }
+    //6. Buat Method ketika FAB Add di click untuk menambahkan data
+    private void onClickFabAdd(){
+        //Cara pertama tambah edit text ke dialog
+        //EditText edtTodo = new EditText(this);
+
+        //Cara dua tambah edit text ke dialog
+        //proses ini disebut dengan inflate layout
+        View view = View.inflate(this,R.layout.dialog_add_view, null);
+
+        //EditText ini dideklarisikan di atas di dalam class
+        edtTodo = view.findViewById(R.id.edt_todo);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Add your list");
+        dialog.setView(view);
+        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                data.add(edtTodo.getText().toString()); // tambah data ke object ArrayList data.
+                arrayAdapter.notifyDataSetChanged(); // merefresh list view
+            }
+        });
+        dialog.setNegativeButton("Cancel",null);
+        dialog.create();
+        dialog.show();
 }
+}
+
